5f3006a66d63dc2c511826d9fd1df22978f133ca
hieplq <hieplq@hasuvimex.vn>
2015-11-20 19:21:58 +0700

    IDEMPIERE-1740:make allocate tab disable when invoice, charge, order is set at head

---
+++ migration/i3.1z/oracle/201401091604-IDEMPIERE-1740.sql
@@ -0,0 +1,10 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Feb 9, 2014 7:26:22 PM ICT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET DisplayLogic='@C_Invoice_ID@=0&@C_Order_ID@=0&@C_Charge_ID@=0',Updated=TO_DATE('2014-02-09 19:26:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=755
+;
+
+SELECT register_migration_script('201401091604-IDEMPIERE-1740.sql') FROM dual
+;
