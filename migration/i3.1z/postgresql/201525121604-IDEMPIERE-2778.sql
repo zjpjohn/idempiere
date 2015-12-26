98321eb9f927a52bc4c05ab3f2d6b21b7480634d
hieplq <hieplq@hasuvimex.vn>
2015-12-26 13:21:21 +0700

    IDEMPIERE-2778:2pack: column size for SQL statement shall be extended

---
+++ migration/i3.1z/postgresql/201525121604-IDEMPIERE-2778.sql
@@ -0,0 +1,189 @@
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=9057
+;
+INSERT INTO t_alter_column values('AD_AlertRule','FromClause','TEXT','not null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=9059
+;
+INSERT INTO t_alter_column values('AD_AlertRule','SelectClause','TEXT','not null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=5937
+;
+INSERT INTO t_alter_column values('PA_MeasureCalc','SelectClause','TEXT','not null','SELECT ... FROM ...')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=146
+;
+INSERT INTO t_alter_column values('AD_Ref_Table','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=147
+;
+INSERT INTO t_alter_column values('AD_Ref_Table','OrderByClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2741
+;
+INSERT INTO t_alter_column values('AD_Tab','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=5938
+;
+INSERT INTO t_alter_column values('PA_MeasureCalc','WhereClause','TEXT','not null','WHERE ...')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=50158
+;
+INSERT INTO t_alter_column values('AD_Package_Exp_Common','SQLStatement','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=12950
+;
+INSERT INTO t_alter_column values('AD_AlertRule','OtherClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15757
+;
+INSERT INTO t_alter_column values('AD_InfoWindow','FromClause','TEXT','not null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15758
+;
+INSERT INTO t_alter_column values('AD_InfoWindow','OtherClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=9058
+;
+INSERT INTO t_alter_column values('AD_AlertRule','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15996
+;
+INSERT INTO t_alter_column values('CM_WikiToken','SelectClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15998
+;
+INSERT INTO t_alter_column values('CM_WikiToken','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15556
+;
+INSERT INTO t_alter_column values('CM_TemplateTable','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15571
+;
+INSERT INTO t_alter_column values('CM_ContainerTTable','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15572
+;
+INSERT INTO t_alter_column values('CM_ContainerTTable','OtherClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15587
+;
+INSERT INTO t_alter_column values('CM_CStageTTable','OtherClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15557
+;
+INSERT INTO t_alter_column values('CM_TemplateTable','OtherClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=54500
+;
+INSERT INTO t_alter_column values('EXP_Format','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=56345
+;
+INSERT INTO t_alter_column values('AD_HouseKeeping','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4396
+;
+INSERT INTO t_alter_column values('AD_ReportView','OrderByClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2742
+;
+INSERT INTO t_alter_column values('AD_Tab','OrderByClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4395
+;
+INSERT INTO t_alter_column values('AD_ReportView','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15586
+;
+INSERT INTO t_alter_column values('CM_CStageTTable','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=208344
+;
+INSERT INTO t_alter_column values('AD_InfoWindow','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=208362
+;
+INSERT INTO t_alter_column values('AD_InfoWindow','OrderByClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=15786
+;
+INSERT INTO t_alter_column values('AD_InfoColumn','SelectClause','TEXT','not null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=210659
+;
+INSERT INTO t_alter_column values('AD_ZoomCondition','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=210565
+;
+INSERT INTO t_alter_column values('AD_ViewComponent','OtherClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=210569
+;
+INSERT INTO t_alter_column values('AD_ViewComponent','FromClause','TEXT','not null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=210568
+;
+INSERT INTO t_alter_column values('AD_ViewComponent','WhereClause','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=59682
+;
+INSERT INTO t_alter_column values('AD_ChartDatasource','FromClause','TEXT','not null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=59685
+;
+INSERT INTO t_alter_column values('AD_ChartDatasource','WhereClause','TEXT','not null','WHERE ...')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=210910
+;
+INSERT INTO t_alter_column values('AD_StatusLine','SQLStatement','TEXT','not null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=210891
+;
+INSERT INTO t_alter_column values('AD_ZoomCondition','ZoomLogic','TEXT','null','NULL')
+;
+
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2015-12-25 22:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=50122
+;
+INSERT INTO t_alter_column values('AD_Package_Exp_Detail','SQLStatement','TEXT','null','NULL')
+;
+
+SELECT register_migration_script('201525121604-IDEMPIERE-2778.sql') FROM dual
+;
+
