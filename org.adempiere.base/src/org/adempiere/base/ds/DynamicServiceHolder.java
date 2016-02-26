93cf84f9ce928afd0b8d2d86f9e05bed873c7f9d
hieplq <hieplq@hasuvimex.vn>
2016-02-26 13:17:39 +0700

    IDEMPIERE-3043:by synchronized. sometime, get list service will wrong

--- org.adempiere.base/src/org/adempiere/base/ds/DynamicServiceHolder.java
+++ org.adempiere.base/src/org/adempiere/base/ds/DynamicServiceHolder.java
@@ -16,12 +16,15 @@ package org.adempiere.base.ds;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 import org.adempiere.base.IServiceHolder;
 import org.adempiere.base.IServicesHolder;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
+import org.python.modules.synchronize;
 
 /**
  * @author hengsin
@@ -30,14 +33,34 @@ import org.osgi.util.tracker.ServiceTracker;
 public class DynamicServiceHolder<T> implements IServiceHolder<T>, IServicesHolder<T> {
 
 	private ServiceTracker<T, T> serviceTracker;
-
+	
+	/**
+	 * each service type have one serviceTracker per application.
+	 * serviceTracker is private at this class, but real it share over application
+	 * so lock object is per service type
+	 */
+	private static Map<Object, Object> lockManage = new Hashtable<> ();
+	
 	/**
 	 * @param tracker
 	 */
 	public DynamicServiceHolder(ServiceTracker<T, T> tracker) {
 		serviceTracker = tracker;
-		if (serviceTracker.getTrackingCount() == -1)
+		// IDEMPIERE-3043
+		Object lock = lockManage.get(tracker);
+		if (lock == null){
+			synchronized (lockManage){
+				// check for late thread
+				lock = lockManage.get(tracker);
+				if (lock == null)
+					lockManage.put(tracker, new Object());
+				lock = lockManage.get(tracker);
+			}
+		}
+		
+		synchronized(lock){
 			serviceTracker.open();
+		}
 	}
 
 	@Override
